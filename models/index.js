cb3ef9c3ec8084604cf8a56354146e6d1499f952
samvrny <samvrny@gmail.com>
2022-08-17 12:32:12 -0500

    finished POST routed and class/constructor

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//create associations between the User and the Post classes/objects
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
