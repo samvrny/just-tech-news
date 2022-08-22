9a558908537dd6b000653185ed61b18acd434860
samvrny <samvrny@gmail.com>
2022-08-22 18:39:15 -0500

    got login and logout functionality settled

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     res.render('homepage', {
         id: 1,
         post_url: 'https://handlebarsjs.com/guide/',
@@ -14,6 +15,14 @@ router.get('/', (req, res) => {
             username: 'test_user'
         }
     });
+});
+
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
