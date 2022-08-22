960835e520fdc571bbcbdfae001eef3e1c8f164a
samvrny <samvrny@gmail.com>
2022-08-22 17:04:40 -0500

    added handlebars and basic front end functionality

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -23,7 +23,10 @@ router.get('/', (req, res) => {
             }
         ]
     })
-    .then(dbPostData => res.json(dbPostData))
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const expressHandlebars = require('express-handlebars');
+const handlebars = expressHandlebars.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //turn on routes
 app.use(routes);
+
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
